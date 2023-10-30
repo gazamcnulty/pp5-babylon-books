a5a6f1bc80a899711ffc073b9379cae71146fc4e
gazamcnulty <gazamcnulty@gmail.com>
2023-10-30 21:42:01 +0000

    added basic versions of Book + Author models, linked on admin

--- babylon_books_app/admin.py
+++ babylon_books_app/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Book, Author
 
 # Register your models here.
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    """
+    Display fields for Book model in Django admin page
+    Decorator ensures superuser access only , not for front end user
+    """
+    list_display = ('title', 'added',)
+    list_filter = ('added',)
+    search_fields = ('title',)
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    """
+    Display fields for Book model in Django admin page
+    Decorator ensures superuser access only , not for front end user
+    """
+    list_display = ('name',)
+    list_filter = ('name',)
+    search_fields = ('name',)
--- babylon_books_app/models.py
+++ babylon_books_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    added = models.DateField()
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+
+    class Meta:
+        ordering = ["-added"]
+
+    def __str__(self):
+        return self.title
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=254)
+    info = models.TextField()
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
