21fc1563edd021493619f1a5f0acc13eba8c8f04
gazamcnulty <gazamcnulty@gmail.com>
2023-11-20 23:03:56 +0000

    added ImageField and sku charfield to Book model

--- babylon_books_app/models.py
+++ babylon_books_app/models.py
@@ -31,6 +31,8 @@ class Book(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     added = models.DateField()
     image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    sku = models.CharField(max_length=254, null=True, blank=True)
 
     class Meta:
         ordering = ["-added"]
