4758bcf756d16af23f3c619755a9092b58af52fe
gazamcnulty <gazamcnulty@gmail.com>
2023-11-20 07:46:21 +0000

    corrected js filepath for stripe card element to show properly

--- checkout/views.py
+++ checkout/views.py
@@ -15,7 +15,7 @@ def checkout(request):
     checkout_template = 'checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_0SMREd7Vdweb1MGRi8S0EycR00JVzSAs5O',
+        'stripe_public_key': 'pk_test_51NxOH6IsJoMM7Nic9H1bA6RzNuMjNpEs4hD7fCY1RphEWQ890sxoVgCH3bcOESj6WDg2MTy4upRUftxdVr10ATh700VTClQWeI',
         'client_secret': 'test client secret',
     }
 
