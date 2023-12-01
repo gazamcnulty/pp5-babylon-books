8c993d902e9fa199fdf9b47cfd67e75428ec6701
gazamcnulty <gazamcnulty@gmail.com>
2023-12-01 10:55:50 +0000

    updated css layout of checkout_success

--- checkout/views.py
+++ checkout/views.py
@@ -172,8 +172,8 @@ def checkout_success(request, order_number):
                 user_profile_form.save()
     
     messages.success(request, f'Order successfully processed! \
-        Your order number is {order_number}. A confirmation \
-        email will be sent to {order.email}.')
+        Your order number is {order_number}. \
+        A confirmation email will be sent to {order.email}.')\
 
     if 'bag' in request.session:
         del request.session['bag']
